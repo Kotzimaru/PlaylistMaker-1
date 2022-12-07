574cb66c423e29f024eb32eda443a4c718a3e698
kotzimaru <kotzimaru@yandex.ru>
2022-12-07 21:33:51 +0300

    pr3

--- app/src/main/java/com/example/playlistmaker1/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker1/SettingsActivity.kt
@@ -1,11 +1,24 @@
 package com.example.playlistmaker1
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+
+
+        val arrowBack = findViewById<Button>(R.id.arrow_back)
+
+
+        arrowBack.setOnClickListener {
+            val mainDisplayIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainDisplayIntent)}
     }
 }
