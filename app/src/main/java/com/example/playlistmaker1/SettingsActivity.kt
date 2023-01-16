78dac16fd0379666af801f4b1ba5c8f4086d22f4
kotzimaru <kotzimaru@yandex.ru>
2023-01-16 10:15:39 +0300

    pr3

--- app/src/main/java/com/example/playlistmaker1/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker1/SettingsActivity.kt
@@ -2,9 +2,11 @@ package com.example.playlistmaker1
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
@@ -12,13 +14,25 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        findViewById<ImageView>(R.id.arrow_back).setOnClickListener {
+            finish()
+        }
+
+        findViewById<ImageView>(R.id.support).setOnClickListener{
+
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("kotzimaru@yandex.ru"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.theme_message))
+            intent.putExtra(Intent.EXTRA_TEXT,getString(R.string.body_message))
+            startActivity(Intent.createChooser(intent,"Send Email"))
+        }
 
 
-        val arrowBack = findViewById<Button>(R.id.arrow_back)
+        findViewById<ImageView>(R.id.offer).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_offer)))
+            startActivity(intent)
+        }
 
-
-        arrowBack.setOnClickListener {
-            val mainDisplayIntent = Intent(this, MainActivity::class.java)
-            startActivity(mainDisplayIntent)}
     }
 }
