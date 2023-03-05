9b995905c2ac92b5a816d313d82242de2e84e769
kotzimaru <kotzimaru@yandex.ru>
2023-03-05 18:25:49 +0300

    sprint11#2

--- app/src/main/java/com/example/playlistmaker1/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker1/SearchActivity.kt
@@ -8,14 +8,14 @@ import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
+import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
@@ -24,31 +24,34 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         var text: String? = "";
     }
+    lateinit var inputEditText: EditText
+    lateinit var clearButton: ImageView
+    lateinit var recyclerView: RecyclerView
+    lateinit var refreshButton: Button
+    lateinit var noConnectError: LinearLayout
+    lateinit var noSearchError: LinearLayout
+    private val appleBaseUrl = "https://itunes.apple.com"
+
+    private val retrofit: Retrofit = Retrofit.Builder().baseUrl(appleBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+
+    private val appleService = retrofit.create<AppleApi>()
+
+    private val track = ArrayList<Track>()
+    private val adapter = TrackAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
-        val clearButton = findViewById<ImageView>(R.id.clearButton)
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        //val placeholderMessage = findViewById(R.id.placeholderMessage)
-        //lateinit var placeholderMessage: TextView
-        val appleBaseUrl = "https://itunes.apple.com/"
-        val refreshButton = findViewById<Button>(R.id.refreshButton)
-        val noSearchError = findViewById<LinearLayout>(R.id.noSearchError)
-        val noConnectError = findViewById<LinearLayout>(R.id.noConnectError)
+        inputEditText = findViewById(R.id.inputEditText)
+        clearButton = findViewById(R.id.clearButton)
+        recyclerView = findViewById(R.id.recycler_view)
+        refreshButton = findViewById(R.id.refreshButton)
+        noSearchError = findViewById(R.id.noSearchError)
+        noConnectError = findViewById(R.id.noConnectError)
+
         recyclerView.layoutManager = LinearLayoutManager(this)
-
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(appleBaseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val appleService = retrofit.create(AppleApi::class.java)
-        val track = ArrayList<Track>()
-        val adapter = TrackAdapter()
 
 
         recyclerView.adapter = adapter
@@ -58,83 +61,35 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        /*fun showMessage(text: String, additionalMessage: String) {
-            if (text.isNotEmpty()) {
-                noSearchError.visibility = View.VISIBLE
-                adapter.deleteList(track, adapter)
-                adapter.notifyDataSetChanged()
-                getString(R.string.nothing_found)
-
-                *//*if (additionalMessage.isNotEmpty()) {
-                    Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-                        .show()
-                }*//*
-            } else {
-                noSearchError.visibility = View.GONE
-            }
-        }*/
-
-
-        fun searchTrack() {
-            appleService.search(inputEditText.text.toString())
-                .enqueue(object : Callback<AppleResponse> {
-                    override fun onResponse(
-                        call: Call<AppleResponse>,
-                        response: Response<AppleResponse>
-                    ) {
-                        if (response.isSuccessful) {
-
-                            if (response.body()?.results?.isNotEmpty() == true) {
-                                adapter.deleteList(track, adapter)
-                                track.addAll(response.body()?.results!!)
-                                recyclerView.visibility = View.VISIBLE
-
-
-                            } else {
-                                noSearchError.visibility = View.VISIBLE
-                                noConnectError.visibility = View.INVISIBLE
-
-
-                            }
-
-                        } else {
-                            noSearchError.visibility = View.INVISIBLE
-                            noConnectError.visibility = View.VISIBLE
-                            refreshButton.visibility = View.VISIBLE
-
-                        }
-                    }
-
-                    override fun onFailure(call: Call<AppleResponse>, t: Throwable) {
-                        t.printStackTrace()
-                        //noSearchError.visibility = View.INVISIBLE
-                        noConnectError.visibility = View.VISIBLE
-                        //refreshButton.visibility = View.VISIBLE
-                    }
-                })
-        }
-
-
-
-
-
-
-        inputEditText.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                searchTrack()// ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
-                true
-            }
-            false
-        }
-
-        refreshButton.setOnClickListener {
-            searchTrack()
+        refreshButton.setOnClickListener{
+            searchTrack(noSearchError, noConnectError)
             noConnectError.visibility = View.INVISIBLE
             noSearchError.visibility = View.INVISIBLE
-            //placeHolderMessage.visibility = View.INVISIBLE
             refreshButton.visibility = View.INVISIBLE
-
         }
+
+        fun visibleInvisibleClearButton(inputEditText: EditText, clear: ImageView) {
+            clear.isVisible = !inputEditText.text.isEmpty()
+        }
+
+        clearButton.setOnClickListener{
+            inputEditText.text.clear()
+            this.currentFocus?.let { view ->
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+                imm?.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+            adapter.deleteList(track, adapter)
+            visibleInvisibleClearButton(inputEditText, clearButton)
+        }
+
+        if (savedInstanceState != null) {
+
+            text = savedInstanceState.getString("textSearch")
+            if (!text.isNullOrEmpty()) {
+                inputEditText.setText(text)
+            }
+        }
+
 
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -149,27 +104,60 @@ class SearchActivity : AppCompatActivity() {
 
         inputEditText.addTextChangedListener(simpleTextWatcher)
 
-        clearButton.setOnClickListener {
-            inputEditText.text.clear()
-            this.currentFocus?.let { view ->
-                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-                imm?.hideSoftInputFromWindow(view.windowToken, 0)
+        inputEditText.setOnEditorActionListener{_, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                searchTrack(noSearchError, noConnectError)// ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
+                true
             }
-            visibleInvisibleClearButton(inputEditText, clearButton)
+            false
         }
 
+    }
+
+
+
+    private fun searchTrack(noSearchError: LinearLayout, noConnectError: LinearLayout) {
+        appleService.search(inputEditText.text.toString())
+            .enqueue(object : Callback<AppleResponse> {
+                override fun onResponse(
+                    call: Call<AppleResponse>,
+                    response: Response<AppleResponse>
+                ) {
+                    if (response.isSuccessful) {
+
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            adapter.deleteList(track, adapter)
+                            track.addAll(response.body()?.results!!)
+                            recyclerView.visibility = View.VISIBLE
+                            adapter.track = track
+                            adapter.notifyDataSetChanged()
+
+
+                        } else {
+                            noSearchError.visibility = View.VISIBLE
+                            noConnectError.visibility = View.INVISIBLE
+                            refreshButton.visibility = View.VISIBLE
+
+                        }
+
+                    } else {
+                        noSearchError.visibility = View.INVISIBLE
+                        noConnectError.visibility = View.VISIBLE
+                        refreshButton.visibility = View.VISIBLE
+
+                    }
+                }
+
+                override fun onFailure(call: Call<AppleResponse>, t: Throwable) {
+                    t.printStackTrace()
+                    noConnectError.visibility = View.VISIBLE
+                    refreshButton.visibility = View.VISIBLE
+                }
+            })
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("textSearch", text)
     }
-
-    fun visibleInvisibleClearButton(inputEditText: EditText, clear: ImageView) {
-
-        clear.isVisible = !inputEditText.text.isEmpty()
-
-    }
-
 }
-
--- app/src/main/java/com/example/playlistmaker1/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker1/TrackViewHolder.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TrackViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parentView.context).inflate(R.layout.track_list_view, parentView, false)
@@ -23,10 +25,11 @@ class TrackViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(
         val cornerRadius = itemView.resources.getDimensionPixelSize(R.dimen.radius_2)
 
 
+
+
         trackName.text = track.trackName
         artistName.text = track.artistName
-        trackTime.text = track.trackTimeMillis
-
+        trackTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis.toLong())
 
 
         Glide.with(itemView)
